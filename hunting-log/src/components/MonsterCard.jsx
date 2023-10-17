cd21e34af22503e443dfe561791d58a63d112cf3
Max Field <maxfield98@outlook.com>
2023-10-17 08:37:11 +0100

    variable name changes

--- hunting-log/src/components/MonsterCard.jsx
+++ hunting-log/src/components/MonsterCard.jsx
@@ -1,6 +1,6 @@
 import ImgGetter from "./ImgGetter";
 
-export default function MonsterCard({ name, species }) {
+export default function MonsterCard({ name }) {
   return (
     <li>
       <h3>{name}</h3>
