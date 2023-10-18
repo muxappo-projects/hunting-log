40ef62ff1f9bdcc61ac2bec257657f5ccf54343b
Max Field <maxfield98@outlook.com>
2023-10-18 23:26:30 +0100

    added bg to list

--- site/src/components/MonsterList.jsx
+++ site/src/components/MonsterList.jsx
@@ -16,18 +16,20 @@ export default function MonsterList({ setMonster, setErrorMsg }) {
   if (isLoading === true) return <p>Loading...</p>;
 
   return (
-    <ul className="monster-list">
-      {monsterList.map((monster) => {
-        return (
-          <MonsterCard
-            key={monster.id}
-            name={monster.name}
-            species={monster.species}
-            setMonster={setMonster}
-            setErrorMsg={setErrorMsg}
-          />
-        );
-      })}
-    </ul>
+    <div className="list-background">
+      <ul className="monster-list">
+        {monsterList.map((monster) => {
+          return (
+            <MonsterCard
+              key={monster.id}
+              name={monster.name}
+              species={monster.species}
+              setMonster={setMonster}
+              setErrorMsg={setErrorMsg}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
