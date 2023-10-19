98271378480453964db841eece6ec6974be601c0
Max Field <maxfield98@outlook.com>
2023-10-19 23:53:58 +0100

    changed from fetch to axios for error handling

--- site/src/api.js
+++ site/src/api.js
@@ -1,16 +1,17 @@
+import request from "axios";
+
 export function fetchMonsterByName(searchTerm) {
-  return fetch(`https://mhw-db.com/monsters?q={"name": "${searchTerm}"}`)
-    .then((response) => response.json())
-    .then((data) => {
-      if (!data || data.length === 0) {
-        throw new Error("Oops! Monster not found!");
-      }
+  return request
+    .get(`https://mhw-db.com/monsters?q={"name": "${searchTerm}"}`)
+    .then(({ data }) => {
       return data;
     });
 }
 
 export function fetchAllMonsters() {
-  return fetch(`https://mhw-db.com/monsters?q={"type": "large"}`).then(
-    (response) => response.json()
-  );
+  return request
+    .get(`https://mhw-db.com/monsters?q={"type": "large"}`)
+    .then(({ data }) => {
+      return data;
+    });
 }
